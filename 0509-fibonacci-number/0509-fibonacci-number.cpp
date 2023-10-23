e2b1305f676ae32cc84e91bb08b061e519c85e02
Abhishek Khare <107669561+abhikhare05@users.noreply.github.com>
2023-10-23 16:44:11 +0530

    Time: 142 ms (5.09%), Space: 6.2 MB (46.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
-    int fib(int n) {
+    
+    int t[31];
+    int solve(int n){
         if(n <= 1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        if(t[n] != -1){
+            return t[n];
+        }
+        return t[n] = fib(n-1) + fib(n-2);
+    }
+    
+    
+    int fib(int n) {
+        /*if(n <= 1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);*/
+        
+        memset(t, -1, sizeof(t));
+        return solve(n);
     }
 };
