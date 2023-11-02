ffdada5d962474bd13f831a95199037aba0003a4
Abhishek Khare <107669561+abhikhare05@users.noreply.github.com>
2023-11-02 08:03:24 +0530

    Time: 11 ms (90.77%), Space: 24.4 MB (83.48%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,33 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    unordered_map<int, int> mp;
     
-    void dfs(TreeNode* root) {
-        if(!root)
+    int currNum = 0;
+    int currFreq = 0;
+    int maxFreq = 0;
+    vector<int> result;
+    
+    void dfs(TreeNode* root){
+        if(!root){
             return;
+        }
         
         dfs(root->left);
-        mp[root->val]++;
+        
+        if(root->val == currNum){
+            currFreq += 1;
+        }
+        else{
+            currNum = root->val;
+            currFreq = 1;
+        }
+        
+        if(currFreq > maxFreq){
+            result = {};
+            maxFreq = currFreq;
+        }
+        
+        if(currFreq == maxFreq){
+            result.push_back(root->val);
+        }
+        
         dfs(root->right);
+        
     }
     
     vector<int> findMode(TreeNode* root) {
         dfs(root);
-        
-        vector<int> result;
-        int maxFreq = 0;
-        
-        for(auto &it : mp) {
-            if(it.second > maxFreq) {
-                maxFreq = it.second;
-                result = {};
-                result.push_back(it.first);
-            } else if (it.second == maxFreq) {
-                result.push_back(it.first);
-            }
-        }
-        
         return result;
-        
     }
 };
